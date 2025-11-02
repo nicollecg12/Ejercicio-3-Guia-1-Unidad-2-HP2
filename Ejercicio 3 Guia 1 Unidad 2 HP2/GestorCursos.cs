726c0e3e2274cea64d08d320de4137daa2e11566
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-02 01:04:56 -0500

    Utilizacion de Singleton

---
+++ Ejercicio 3 Guia 1 Unidad 2 HP2/GestorCursos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_Guia_1_Unidad_2_HP2
+{
+    public class GestorCursos
+    {
+        public static GestorCursos _instancia;
+        private List<Cursos> cursos;
+
+        public GestorCursos()
+        {
+            cursos = new List<Cursos> { };
+        }
+
+        public static GestorCursos Instancia
+        {
+            get
+            {
+                if(_instancia == null)
+                {
+                    _instancia = new GestorCursos();
+                }
+                return _instancia;
+            }
+        }
+
+        public void AñadirCurso(Cursos curso)
+        {
+            cursos.Add(curso);
+        }
+        public List<Cursos> ObtenerCurso()
+        {
+            return cursos;
+        }
+
+        public void Eliminar()
+        {
+            cursos.Clear();
+        }
+
+    }
+}
