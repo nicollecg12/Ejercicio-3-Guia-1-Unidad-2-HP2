5b80e9fe76b328ba09f0aa1a43c495ad26a44d7e
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-02 01:05:39 -0500

    Clase derivada tipo de curso presencial

---
+++ Ejercicio 3 Guia 1 Unidad 2 HP2/Presencial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_Guia_1_Unidad_2_HP2
+{
+    public class Presencial:Cursos
+    {
+        public double CostoAdicional { get; set; }
+
+        public Presencial(int numeroEstudiantes, string nombreCurso, string nombreDocente, int duracionCurso, double precioCurso) : base (numeroEstudiantes, nombreCurso, nombreDocente, duracionCurso, precioCurso) 
+        {
+            if (numeroEstudiantes > 30)
+            {
+                CostoAdicional = precioCurso * 0.2;
+            }
+            else
+            {
+                CostoAdicional = 0;
+            }
+        }
+
+        public override double CalcularPrecio()
+        {
+            return PrecioCurso + CostoAdicional;
+        }
+
+
+    }
+}
