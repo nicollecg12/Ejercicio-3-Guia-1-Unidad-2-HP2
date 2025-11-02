db4d0e5c8b36f688fef879d7a39d372825ec4bd6
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-02 01:04:18 -0500

    Utilizacion de Factory para creación de cursos

---
+++ Ejercicio 3 Guia 1 Unidad 2 HP2/CursosFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_Guia_1_Unidad_2_HP2
+{
+    public class CursosFactory
+    {
+        public static Cursos CrearCurso(string tipo, int numeroEstudiantes, string nombreCurso, string nombreDocente, int duracionCurso, double precioCurso)
+        {
+            if (tipo == "Presencial")
+            {
+                return new Presencial(numeroEstudiantes,nombreCurso ,nombreDocente, duracionCurso, precioCurso);
+            }
+            else if (tipo == "Virtual")
+            {
+                return new Virtual(numeroEstudiantes, nombreCurso, nombreDocente, duracionCurso, precioCurso);
+            }
+            else
+            {
+                throw new ArgumentException("Tipo de curso no valido");
+            }
+        }
+    }
+}
