4ad568821379242113e9b6c37c93bd5160b932bd
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-02 01:05:22 -0500

    Clase derivada tipo de curso virtual

---
+++ Ejercicio 3 Guia 1 Unidad 2 HP2/Virtual.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_Guia_1_Unidad_2_HP2
+{
+    public class Virtual : Cursos
+    {
+        public double Descuento { get; set; }
+
+        public Virtual(int numeroEstudiantes, string nombreCurso, string nombreDocente, int duracionCurso, double precioCurso) : base(numeroEstudiantes, nombreCurso, nombreDocente, duracionCurso, precioCurso)
+        {
+            if (numeroEstudiantes > 50)
+            {
+                Descuento = precioCurso * 0.15;
+            }
+            else
+            {
+                Descuento = 0;
+            }
+        }
+
+        public override double CalcularPrecio()
+        {
+            return PrecioCurso - Descuento;
+        }
+    }
+}
